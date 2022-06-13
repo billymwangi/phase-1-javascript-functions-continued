a3525a18cf5dcb5aaee59cb8f5f05148d722ab29
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-13 13:57:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function SaturdayFun(defaultActivity="roller-skate"){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+
+function mondayWork(defaultActivity = "go to the office"){
+    return `This Monday, I will ${defaultActivity}.`
+}
+
+function wrapAdjective(result ="||"){
+    return function (emphatic = "a dedicated programmer"){
+    return  `You are ${result}$[emphatic]${result}!`
+    }
+}
