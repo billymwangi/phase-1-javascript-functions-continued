e84e9325a520a82b3a841e9ee615d82794cf795d
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-13 14:00:21 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 // code your solution here
-function SaturdayFun(defaultActivity="roller-skate"){
-    return `This Saturday, I want to ${defaultActivity}!`
-}
+
 
 function mondayWork(defaultActivity = "go to the office"){
     return `This Monday, I will ${defaultActivity}.`
